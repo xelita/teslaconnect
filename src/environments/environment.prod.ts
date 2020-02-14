b3cdaee6d546fd45d5a44d5bf624599aa5ec4cc1
xelita <dev@xelita.com>
2020-02-14 23:50:50 +0100

    responsiveness

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  tesla_oauth_base_url: 'http://localhost:8080/oauth',
+  tesla_api_base_url: 'http://localhost:8080/api/1',
+  tesla_api_client_id: '81527cff06843c8634fdc09e8ac0abefb46ac849f38fe1e431c2ef2106796384',
+  tesla_api_client_secret: 'c7257eb71a564034f9419ee651c7d0e5f7aa6bfbd18bafb5c5c033b093bb2fa3',
+  email: '',
+  password: '',
+  auth_token: ''
 };
