cdbba6dcc1d4f5323c87c532edf0fd80a520fb65
xelita <dev@xelita.com>
2020-02-17 19:11:59 +0100

    adding buildspec.yml file to allow AWS deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  tesla_oauth_base_url: 'http://localhost:8080/oauth',
-  tesla_api_base_url: 'http://localhost:8080/api/1',
+  tesla_oauth_base_url: 'http://35.180.207.116:8080/oauth',
+  tesla_api_base_url: 'http://35.180.207.116:8080/api/1',
   tesla_api_client_id: '81527cff06843c8634fdc09e8ac0abefb46ac849f38fe1e431c2ef2106796384',
   tesla_api_client_secret: 'c7257eb71a564034f9419ee651c7d0e5f7aa6bfbd18bafb5c5c033b093bb2fa3',
   email: '',
